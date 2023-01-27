31a8bb50b4b51f7239ca611a4c7fe82b95e8d9f4
Victor Vat <victor16@inbox.ru>
2023-01-27 21:55:12 +0300

    Showing the Posts List

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react'
+
+const Navbar: FunctionComponent = () => {
+  return (
+    <nav>
+      <section>
+        <h1>Redux Essentials Example</h1>
+
+        <div className="navContent">
+          <div className="navLinks"></div>
+        </div>
+      </section>
+    </nav>
+  )
+}
+
+export default Navbar;
