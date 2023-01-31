2e7814c51e3d977b77521f1694d2ec24a7117aea
Victor Vat <victor16@inbox.ru>
2023-01-31 16:19:44 +0300

    api points separated by mind

--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import postsReducer from '../features/posts/postsSlice';
+// import postsReducer from '../features/posts/postsSlice';
 // import usersReducer from '../features/users/usersSlice';
 import notificationReducer from '../features/notifications/notificationsSlice';
 import { apiSlice } from '../features/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
-    posts: postsReducer,
+    // posts: postsReducer,
     // users: usersReducer,
     notifications: notificationReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
